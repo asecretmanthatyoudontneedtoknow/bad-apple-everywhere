2bea6cce8d7860dee5d423a8d2fb25c707fd39e6
asecretmanthatyoudontneedtoknow <leizarksigma@gmail.com>
2026-04-25 20:14:36 +0500

    Update ascii_player.py

--- ascii_player.py
+++ ascii_player.py
@@ -78,7 +78,7 @@ def play_ascii_video():
             current_time = time.time()
             
             if midi_path and not music_started:
-                if (current_time - start_time) >= 0.5:
+                if (current_time - start_time) >= 1.0:
                     pygame.mixer.music.play()
                     music_started = True
 
